2a7a68ffa1c1a55845aefb44e70724e836cb0b08
zaycation <contact@zaycation.dev>
2020-09-03 12:37:44 -0700

    removed 'server-info'

--- commands/server-info.js
+++ commands/server-info.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'server-info',
 	description: 'Display info about this server.',
 	execute(message) {
-		message.channel.send(`Server name: ${message.guild.name}\nRegion: ${message.guild.region}\nTotal members: ${message.guild.memberCount}\nServer Owner: ${message.guild.owner} | ${message.guild.ownerID}\nNumber of Boosts: ${message.guild.premiumSubscriptionCount} | ${message.guild.premiumTier}`);
+		message.channel.send(`${message.guild.name}\nRegion: ${message.guild.region}\nTotal members: ${message.guild.memberCount}\nServer Owner: ${message.guild.owner} | ${message.guild.ownerID}\nNumber of Boosts: ${message.guild.premiumSubscriptionCount} | ${message.guild.premiumTier}`);
 	},
 };
