af8e43bccab2014eaeb402df7cb450e2b8ed7bf8
zaycation <contact@zaycation.dev>
2020-09-03 12:28:53 -0700

    added timestamp + avi link

--- commands/user-info.js
+++ commands/user-info.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'user-info',
 	description: 'Display info about yourself.',
 	execute(message) {
-		message.channel.send(`Your username: ${message.author.username}\nYour locale: ${message.author.locale}\nYour ID: ${message.author.id}`);
+		message.channel.send(`Your username: ${message.author.username}\nCreated: ${message.author.createdTimestamp}\nYour ID: ${message.author.id}\n${message.author.displayAvatarURL({ dynamic: true })}`);
 	},
 };
