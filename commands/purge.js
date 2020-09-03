a22d9a380e4ad835502878b2d798723c07cf844a
zaycation <contact@zaycation.dev>
2020-09-03 12:58:19 -0700

    added return msg and maximum 500

--- commands/purge.js
+++ commands/purge.js
@@ -1,14 +1,15 @@
   
 module.exports = {
 	name: 'purge',
-	description: 'Prune up to 99 messages.',
+	description: 'Prune up to 500 messages.',
 	execute(message, args) {
 		const amount = parseInt(args[0]) + 1;
+		return message.reply('hella shit jus got deleted bruv 0.o');
 
 		if (isNaN(amount)) {
 			return message.reply('that doesn\'t seem to be a valid number.');
-		} else if (amount <= 1 || amount > 100) {
-			return message.reply('you need to input a number between 1 and 99.');
+		} else if (amount <= 1 || amount > 500) {
+			return message.reply('you need to input a number between 1 and 500.');
 		}
 
 		message.channel.bulkDelete(amount, true).catch(err => {
