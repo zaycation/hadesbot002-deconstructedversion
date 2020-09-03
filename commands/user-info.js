68b24e363350b025dcf05c4d4106f80abe64dfd3
zaycation <contact@zaycation.dev>
2020-09-03 12:30:54 -0700

    edited created time

--- commands/user-info.js
+++ commands/user-info.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'user-info',
 	description: 'Display info about yourself.',
 	execute(message) {
-		message.channel.send(`Your username: ${message.author.username}\nCreated: ${message.author.createdTimestamp}\nYour ID: ${message.author.id}\n${message.author.displayAvatarURL({ dynamic: true })}`);
+		message.channel.send(`Your username: ${message.author.username}\nCreated: ${message.author.createdAt}\nYour ID: ${message.author.id}\n${message.author.displayAvatarURL({ dynamic: true })}`);
 	},
 };
