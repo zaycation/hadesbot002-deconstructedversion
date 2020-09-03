eb73505716003f18bce1da386e3cfab3fcc0e2b0
zaycation <contact@zaycation.dev>
2020-09-03 12:50:09 -0700

    spacing edits for display on discord

--- commands/user-info.js
+++ commands/user-info.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'user-info',
 	description: 'Display info about yourself.',
 	execute(message) {
-		message.channel.send(`${message.author.username}\nYour ID: ${message.author.id}\n Is User A Bot? ${message.author.bot}\nCreated: ${message.author.createdAt}\nLast Message: > ${message.author.lastMessage}\nUser's Avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
+		message.channel.send(`Username: ${message.author.username}\nYour ID: ${message.author.id}\nCreated: ${message.author.createdAt}\n\n Is User A Bot? ${message.author.bot}\nLast Message: >${message.author.lastMessage}\n\nUser's Avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
 	},
 };
