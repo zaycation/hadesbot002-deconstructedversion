f72b7a63d52701b4783ee1232334355eff7b74d2
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-04 15:56:46 -0700

    embed for user-info

--- commands/user-info.js
+++ commands/user-info.js
@@ -2,6 +2,9 @@ module.exports = {
 	name: 'user-info',
 	description: 'Display info about yourself.',
 	execute(message) {
-		message.channel.send(`Username: ${message.author.username}\nYour ID: ${message.author.id}\nCreated: ${message.author.createdAt}\n\nIs User A Bot? ${message.author.bot}\n\nLast Message: \n> ${message.author.lastMessage}\n\nUser's Avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
-	},
+        message.channel.send({embed: {
+            color: 3447003,
+            description: `Username: ${message.author.username}\nYour ID: ${message.author.id}\nCreated: ${message.author.createdAt}\n\nIs User A Bot? ${message.author.bot}\n\nLast Message: \n> ${message.author.lastMessage}\n\nUser's Avatar: ${message.author.displayAvatarURL({ dynamic: true })}`
+          }});
+    },
 };
