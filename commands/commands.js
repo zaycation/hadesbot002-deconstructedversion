a5354a8366b9f1ba00dde50bd695e0acfdd96821
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-04 14:45:54 -0700

    editing commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -1,11 +1,19 @@
 module.exports = {
 	name: 'commands',
-    description: 'List of all commands',
+    description: 'Quick list of all commands in the chat',
     args: true,
-    cooldown: 2,
+    cooldown: 5,
 	execute(message, args) {
-		message.reply('Here\'s a list of all my commands:\n\n`[Always remember, my prefix is !]`\n\n');
+
+        const data = [];
+        const { commands } = message.client
+
+		data.push('Here\'s a list of all my commands:\n\n`[Always remember, my prefix is !]`\n\n');
 		data.push(commands.map(command => command.name).join(', '));
-		data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+        data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+        
+        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+
+		message.channel.send(data, { split: true });
 	},
 };
