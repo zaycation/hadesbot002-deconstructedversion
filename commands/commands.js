ea9bf7cb3d8c2e11a038b7bbf782718e57bde9bf
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-04 15:52:17 -0700

    added replies and more simple embeds

--- commands/commands.js
+++ commands/commands.js
@@ -3,7 +3,10 @@ module.exports = {
     description: 'Quick list of all commands in the chat',
     args: true,
     cooldown: 5,
-	execute(message, args) {
-		message.reply('Here\'s a list of all my commands:\n\n`[Always remember, my prefix is !]`\n\n!args-info\n!avatar\n!help\n!kick\n!ping\n!purge\n!server-info\n!user-info\n\nAlways remember you can run `!help <command>` for additional info on how each command works');
-	},
+	execute(message) {
+        message.channel.send({embed: {
+            color: 3447003,
+            description: 'Here\'s a list of all my commands:\n\n`[Always remember, my prefix is !]`\n\n!args-info\n!avatar\n!help\n!kick\n!ping\n!purge\n!server-info\n!user-info\n\nAlways remember you can run `!help <command>` for additional info on how each command works'
+          }});
+    },
 };
--- commands/server-info.js
+++ commands/server-info.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'server-info',
 	description: 'Display info about this server.',
 	execute(message) {
-		message.channel.send(`${message.guild.name}\nRegion: ${message.guild.region}\nTotal members: ${message.guild.memberCount}\nNumber of Boosts: ${message.guild.premiumSubscriptionCount} | Boost Tier: ${message.guild.premiumTier}\nServer Owner: ${message.guild.owner} | ID: ${message.guild.ownerID}`);
+		message.reply(`${message.guild.name}\nRegion: ${message.guild.region}\nTotal members: ${message.guild.memberCount}\nNumber of Boosts: ${message.guild.premiumSubscriptionCount} | Boost Tier: ${message.guild.premiumTier}\nServer Owner: ${message.guild.owner} | ID: ${message.guild.ownerID}`);
 	},
 };
