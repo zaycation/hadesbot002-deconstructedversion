cf368eb2b84f2397055562eb96f50ae1671cb6e6
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-04 14:18:58 -0700

    revised ping.js as template

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,15 @@
-module.exports = {
-	name: 'ping',
-	description: 'Ping!',
+const Discord = require('discord.js')
+
+module.exports.run = {
 	execute(message) {
 		message.channel.send('Pong.');
 	},
 };
+
+module.exports.config = {
+	name: 'ping',
+	description: 'Play ping pong with Hades.',
+	usage: '!ping',
+	accesableby: 'Members',
+	aliases: ['p1']
+}
