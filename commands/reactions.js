510e75e9b62d3b4a43f65784b084dfa521433e13
zaycation <contact@zaycation.dev>
2020-09-04 10:46:11 -0700

    added code for basic reation

--- commands/reactions.js
+++ commands/reactions.js
@@ -2,6 +2,11 @@ module.exports = {
 	name: 'reactions',
 	description: 'Set up reaction roles for server.',
 	execute(message) {
-		message.channel.send('Pong.');
+		let embed = new Discord.MessageEmbed()
+		.setTitle('Reaction Roles')
+		.setDescription('React to obtain your role of choice')
+		.setColor('Green')
+		let msgEmbed = await message.channel.send(embed).
+		msgEmbed.react('🐢')
 	},
 };
