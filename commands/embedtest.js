b3fbed2ebe20a18c9f7066f21665a29664e85705
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-04 15:40:08 -0700

    simple embed added for embedtest.js

--- commands/embedtest.js
+++ commands/embedtest.js
@@ -1,17 +1,13 @@
-const Discord = require('discord.js');
-
 module.exports = {
     name: 'embedtest',
     description: 'Command created to test embed functionality of Hades',
     execute(message, bot, args) {
 
 
-        const exampleEmbed = new Discord.MessageEmbed()
-            .setTitle('Some title')
-            .attachFiles(['../assets/discordjs.png'])
-            .setImage('attachment://discordjs.png');
-
-        channel.send(exampleEmbed);
+        message.channel.send({embed: {
+            color: 3447003,
+            description: "A very simple Embed!"
+          }});
 
 
     },
