15a15c5573ad053ebeaf14ea07019687b7709fa8
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-04 14:49:35 -0700

    fixed code in commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -4,16 +4,6 @@ module.exports = {
     args: true,
     cooldown: 5,
 	execute(message, args) {
-
-        const data = [];
-        const { commands } = message.client
-
-		data.push('Here\'s a list of all my commands:\n\n`[Always remember, my prefix is !]`\n\n');
-		data.push(commands.map(command => command.name).join(', '));
-        data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
-        
-        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-
-		message.channel.send(data, { split: true });
+		message.reply('Here\'s a list of all my commands:\n\n`[Always remember, my prefix is !]`\n\n!args-info\n!avatar\n!help\n!kick\n!ping\n!purge\n!server-info\n!user-info');
 	},
 };
